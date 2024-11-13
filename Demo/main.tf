7d14fbb91400c39130df13ab90d6f7887a022c68
Emmanuel Oluwafemi <fasogbaemmanuel@gmail.com>
2024-11-13 14:14:32 +0100

    Add tags to resources

--- Demo/main.tf
+++ Demo/main.tf
@@ -3,10 +3,12 @@ provider "aws" {
 }
 resource "aws_vpc" "devops-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags       = { "Name" : "demo-vpc" }
 }
 
 resource "aws_subnet" "devops-subnet" {
   vpc_id            = aws_vpc.devops-vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
+  tags              = { "Name" : "demo-subnet" }
 }
