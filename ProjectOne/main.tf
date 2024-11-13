b3019cf3a868b659b75839048d25917cef794640
Emmanuel Oluwafemi <fasogbaemmanuel@gmail.com>
2024-11-13 23:56:49 +0100

    Add cidr blocks variables

---
+++ ProjectOne/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "cidr block value for vpc"
+}
+
+variable "subnet_cidr_block" {
+  description = "cidr block value for subnet"
+}
+
+variable "az" {
+  description = "Availability zone where subnet would be created"
+}
