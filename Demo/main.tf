c12eb300b5cc379067203dc7a44732179cbc8342
Emmanuel Oluwafemi <fasogbaemmanuel@gmail.com>
2024-11-13 14:12:16 +0100

    Add Subnet resource to configuration

--- Demo/main.tf
+++ Demo/main.tf
@@ -4,3 +4,9 @@ provider "aws" {
 resource "aws_vpc" "devops-vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "devops-subnet" {
+  vpc_id            = aws_vpc.devops-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+}
