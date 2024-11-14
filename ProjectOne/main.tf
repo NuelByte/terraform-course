b8e4daf0e29df86e42a9a3bb9fde596be06f8203
Emmanuel Oluwafemi <fasogbaemmanuel@gmail.com>
2024-11-14 12:49:09 +0100

    Add internet gateway

--- ProjectOne/main.tf
+++ ProjectOne/main.tf
@@ -54,6 +54,10 @@ resource "aws_route_table" "project_one_route_table" {
   vpc_id = aws_vpc.project_one_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "we don't have one yet"
+    gateway_id = aws_internet_gateway.project_one_igw.id
   }
+}
+
+resource "aws_internet_gateway" "project_one_igw" {
+  vpc_id = aws_vpc.project_one_vpc.id
 }
