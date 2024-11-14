3866ad49809e7044726ffc9231b889774b152fb0
Emmanuel Oluwafemi <fasogbaemmanuel@gmail.com>
2024-11-14 14:48:04 +0100

    Reference default route table

--- ProjectOne/main.tf
+++ ProjectOne/main.tf
@@ -65,6 +65,15 @@ resource "aws_internet_gateway" "project_one_igw" {
 }
 
 resource "aws_route_table_association" "project-one-rta" {
-  subnet_id = aws_subnet.project_one_subnet_1.id
+  subnet_id      = aws_subnet.project_one_subnet_1.id
   route_table_id = aws_route_table.project_one_route_table.id
 }
+
+# resource "aws_default_route_table" "project_one_main_rtb" {
+#   default_route_table_id = aws_vpc.project_one_vpc.default_route_table_id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.project_one_igw.id
+#   }
+#   tags = { "Name" : "${var.env.prefix}_main_rtb" }
+# }
